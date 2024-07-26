9b7f79b7edf23cd09f7b0c2aa2105517970ec8e5
viktorkrustev <viktorkrustev03@abv.bg>
2024-07-26 19:20:18 +0300

    commit

--- OnlineShop-App/src/main/java/com/onlineShop/init/FirstInit.java
+++ OnlineShop-App/src/main/java/com/onlineShop/init/FirstInit.java
@@ -2,31 +2,35 @@ package com.onlineshop.init;
 
 import com.onlineshop.model.entity.Role;
 import com.onlineshop.model.entity.User;
-import com.onlineshop.repository.ProductRepository;
-import com.onlineshop.repository.UserRepository;
 import com.onlineshop.service.UserService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+
 @Component
 public class FirstInit implements CommandLineRunner {
+
     private final PasswordEncoder passwordEncoder;
-
     private final UserService userService;
+    private final DataSource dataSource;
 
-    public FirstInit(PasswordEncoder passwordEncoder, UserService userService) {
+    @Autowired
+    public FirstInit(PasswordEncoder passwordEncoder, UserService userService, DataSource dataSource) {
         this.passwordEncoder = passwordEncoder;
         this.userService = userService;
+        this.dataSource = dataSource;
     }
-
 
     @Override
     @Transactional
     public void run(String... args) {
-        if (!userService.existsByUsername("Viktor")){
+        if (!userService.existsByUsername("Viktor")) {
             User user = new User();
             user.setFirstName("Viktor");
             user.setLastName("Krustev");
@@ -35,7 +39,13 @@ public class FirstInit implements CommandLineRunner {
             user.setRole(Role.ADMIN);
             user.setUsername("Viktor");
             userService.saveUser(user);
-
+            executeSqlScripts();
         }
+    }
+
+    private void executeSqlScripts() {
+        ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
+        resourceDatabasePopulator.addScript(new ClassPathResource("data.sql"));
+        resourceDatabasePopulator.execute(dataSource);
     }
 }
