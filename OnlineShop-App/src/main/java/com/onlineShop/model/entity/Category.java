51b714b82df3efd728dfec34c848f384f0a17c6d
viktorkrustev <viktorkrustev03@abv.bg>
2024-08-07 22:49:49 +0300

    commit

--- OnlineShop-App/src/main/java/com/onlineShop/model/entity/Category.java
+++
@@ -1,9 +0,0 @@
-package com.onlineShop.model.entity;
-
-public enum Category {
-    LAPTOPS,
-    WATCHES,
-    PHONES,
-    HEADPHONES,
-    TELEVISIONS
-}
--- OnlineShop-App/src/main/java/com/onlineShop/model/entity/Role.java
+++
@@ -1,5 +0,0 @@
-package com.onlineShop.model.entity;
-
-public enum Role {
-    USER, ADMIN
-}
